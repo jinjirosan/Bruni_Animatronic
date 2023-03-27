b9505d124d7f8122693b7ed46606f29d75d072d2
Jinjiro-San <jinjirosan@users.noreply.github.com>
2023-03-27 14:11:46 +0200

    v4-1.0 (pre-release) - refactor C2.0.3

--- bruni_code.py
+++ bruni_code.py
@@ -7,7 +7,7 @@
 #
 # (2023) JinjiroSan
 #
-# bruni_code.py : v4-1.0 (pre-release) - refactor C2.0.2
+# bruni_code.py : v4-1.0 (pre-release) - refactor C2.0.3
 
 import machine
 import utime
@@ -63,107 +63,112 @@ class ButtonState:
 button_state = ButtonState()
 
 def tail_wag():
-    print("Starting tail wagging")
+    # Iterate over the three positions of the servo: left, right, and center
     for angle in [neutral_angle - angle_change, neutral_angle + angle_change, neutral_angle]:
-        pwm.duty_ns(int(angle / 180 * (MAX - MIN) / 2 + MID))
-        utime.sleep(0.5 if angle in [neutral_angle - angle_change, neutral_angle + angle_change, neutral_angle] else 0.2)
+        pwm.duty_ns(int(angle / 180 * (MAX - MIN) / 2 + MID))    # Calculate the PWM duty cycle for the current angle and set it for the servo
+        utime.sleep(0.5 if angle in [neutral_angle - angle_change, neutral_angle + angle_change, neutral_angle] else 0.2)    # Wait for 0.5 seconds for left and right positions, and 0.2 seconds for center position to create a smooth animation
+
 
 def tail_wag_random():
     global tail_wagging
-    tail_wagging = True
-    count = 0
-    while count < wag_count:
-        count += 1
-        print("Tail wagging {}/{}".format(count, wag_count))
-        tail_wag()
-        if count == wag_count:
+    tail_wagging = True    # Set the tail_wagging flag to True to indicate that the tail is currently wagging
+    count = 0              # Initialize the count of tail wags to 0
+    while count < wag_count:   # Repeat the following block of code until wag_count tail wags have been performed
+        count += 1              # Increment the count of tail wags
+        print("Tail wagging {}/{}".format(count, wag_count))    # Print a message to indicate which tail wag is currently being performed
+        tail_wag()           # Call the tail_wag() function to perform a single tail wag
+        if count == wag_count:   # If all tail wags have been performed, break out of the while loop
             break
         else:
-            wait_duration = random.randint(*wait_range) 
-            print("Waiting for {} seconds until next tail wag...".format(wait_duration))
-            utime.sleep(wait_duration)
+            wait_duration = random.randint(*wait_range)     # Generate a random wait duration between the specified range of values
+            print("Waiting for {} seconds until next tail wag...".format(wait_duration))    # Print a message to indicate how long the wait will be
+            utime.sleep(wait_duration)    # Wait for the specified duration before performing the next tail wag
 
-    pwm.duty_ns(MID)
-    print("Tail in neutral position, waiting for next button press...")
-    tail_wagging = False  # set tail_wagging flag back to False
+    pwm.duty_ns(MID)    # Set the servo to the neutral position
+    print("Tail in neutral position, waiting for next button press...")   # Print a message to indicate that the tail is now in the neutral position and waiting for the next button press
+    tail_wagging = False  # Set the tail_wagging flag back to False to indicate that the tail is no longer wagging
+
 
 
 
 def button_wag(pressed_button_pin):
-    last_press_time = 0
-    wag_count = 0
+    last_press_time = 0     # Initialize the timestamp of the last button press to 0
+    wag_count = 0           # Initialize the count of tail wags to 0
 
-    while True:
-        if not pressed_button_pin.value():
-            current_time = utime.time()
-            if current_time - last_press_time < 60 and wag_count < 1:
-                tail_wag()
-                wag_count += 1
-                print("Tail wagging 1/{}".format(wag_count))
-                tail_wag_random()
-                last_press_time = current_time
-                wag_count = 0
+    while True:             # Repeat the following block of code indefinitely
+        if not pressed_button_pin.value():  # If the button is pressed (i.e., its value is False)
+            current_time = utime.time()     # Get the current timestamp
+            if current_time - last_press_time < 60 and wag_count < 1:  # If less than 60 seconds have elapsed since the last button press and fewer than 1 tail wags have been performed
+                tail_wag()                    # Call the tail_wag() function to perform a single tail wag
+                wag_count += 1                # Increment the count of tail wags performed
+                print("Tail wagging 1/{}".format(wag_count))  # Print a message to indicate which tail wag is currently being performed
+                tail_wag_random()             # Call the tail_wag_random() function to perform the remaining tail wags
+                last_press_time = current_time    # Update the timestamp of the last button press to the current time
+                wag_count = 0                 # Reset the count of tail wags to 0
 
-            elif current_time - last_press_time >= 60:
-                wag_count = 0
-                last_press_time = current_time
+            elif current_time - last_press_time >= 60:    # If 60 seconds or more have elapsed since the last button press
+                wag_count = 0                  # Reset the count of tail wags to 0
+                last_press_time = current_time  # Update the timestamp of the last button press to the current time
+
 
 def flame_effect():
-    global led_on
-    if led_on:
-        color = random.choice(COLORS)
-        brightness = random.randint(MIN_BRIGHTNESS, MAX_BRIGHTNESS)
-        led[0] = tuple(map(lambda x: int(x * brightness / 255), color))
+    global led_on           # Access the global variable led_on
+    if led_on:              # If the LED is on
+        color = random.choice(COLORS)                # Choose a random color from the list of available colors
+        brightness = random.randint(MIN_BRIGHTNESS, MAX_BRIGHTNESS)   # Choose a random brightness level within the specified range
+        led[0] = tuple(map(lambda x: int(x * brightness / 255), color))   # Set the brightness level for the chosen color and write it to the LED strip
         led.write()
-        return random.uniform(0.05, 0.2)
-    else:
-        led[0] = (0, 0, 0)
+        return random.uniform(0.05, 0.2)    # Return a random wait time between 0.05 and 0.2 seconds
+    else:                   # If the LED is off
+        led[0] = (0, 0, 0)  # Set the LED strip to be completely off
         led.write()
-        return 0.1
+        return 0.1          # Return a wait time of 0.1 seconds
+
 
 
 def button_pressed(pin):
-    global led_on  # Add led_on to the global variables
-    current_time = utime.ticks_ms()
-    if utime.ticks_diff(current_time, button_state.last_press_time) > debounce_time:
-        print("Flame effect button pressed")
-        button_state.last_press_time = current_time
-        if led_on:
-            led_on = False
-        else:
-            led_on = True
-        flame_effect()  # Add this line to call the flame_effect function
+    global led_on          # Access the global variable led_on
+    current_time = utime.ticks_ms()    # Get the current timestamp in milliseconds
+    if utime.ticks_diff(current_time, button_state.last_press_time) > debounce_time:   # If the time difference between the current and last button presses is greater than the debounce time
+        print("Flame effect button pressed")     # Print a message to indicate that the button has been pressed
+        button_state.last_press_time = current_time   # Update the timestamp of the last button press to the current time
+        if led_on:              # If the LED is on
+            led_on = False      # Turn it off
+        else:                   # If the LED is off
+            led_on = True       # Turn it on
+        flame_effect()          # Call the flame_effect() function to update the LED strip accordingly
 
 
-led_on = False
+
+led_on = False              # Initialize the LED state to be off
 flame_thread = None
 
 def button_wag_handler(pin):
-    global wag_event
-    wag_event = True
-    if not tail_wagging:
-        _thread.start_new_thread(tail_wag_random, ())  # Start the tail_wag_random in a new thread
+    global wag_event        # Access the global variable wag_event
+    wag_event = True        # Set the wag_event flag to True to indicate that the button has been pressed
+    if not tail_wagging:    # If the tail is not already wagging
+        _thread.start_new_thread(tail_wag_random, ())  # Start the tail_wag_random function in a new thread to wag the tail
 
+button_led_pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=button_pressed)   # Register the button_pressed function to handle the falling edge interrupt on the button LED pin
 
-button_led_pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=button_pressed)
-
-button_wag_pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=button_wag_handler)
+button_wag_pin.irq(trigger=machine.Pin.IRQ_FALLING, handler=button_wag_handler)    # Register the button_wag_handler function to handle the falling edge interrupt on the button wag pin
 
 while True:
-    if wag_event:
-        wag_event = False
-        if not tail_wagging:
-            tail_wagging = True
-            tail_wag_random()
-            tail_wagging = False
+    if wag_event:       # If the wag button has been pressed
+        wag_event = False   # Reset the wag_event flag
+        if not tail_wagging:    # If the tail is not already wagging
+            tail_wagging = True     # Set the tail_wagging flag to True to indicate that the tail is wagging
+            tail_wag_random()       # Call the tail_wag_random function to wag the tail randomly
+            tail_wagging = False    # Set the tail_wagging flag back to False to indicate that the tail is not wagging anymore
 
-    if flame_event:
-        flame_event = False
-        if led_on:
-            led_on = False
-        else:
-            led_on = True
+    if flame_event:     # If the flame button has been pressed
+        flame_event = False     # Reset the flame_event flag
+        if led_on:              # If the LED is currently on
+            led_on = False      # Turn it off
+        else:                   # If the LED is currently off
+            led_on = True       # Turn it on
 
-    wait_time = flame_effect()
-    utime.sleep(wait_time)
-    utime.sleep(0.1)
+    wait_time = flame_effect()  # Call the flame_effect function to update the LED strip and get the wait time
+    utime.sleep(wait_time)      # Wait for the specified amount of time
+    utime.sleep(0.1)            # Add a small delay between iterations to avoid busy waiting
+
